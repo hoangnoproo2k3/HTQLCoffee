c2e4b84a4798bc53cba15db07c2bfa6f02b34fca
Viet Hoang <hoangproo2624@gmail.com>
2024-04-05 22:25:05 +0700

    mvc

--- HTQLCoffee/Controller/NguyenLieuController.cs
+++ HTQLCoffee/Controller/NguyenLieuController.cs
@@ -10,39 +10,16 @@ namespace HTQLCoffee.Controller
 {
     internal class NguyenLieuController
     {
-        private readonly string _connectionString;
-        //hàm tạo(constructor)
+        private readonly NguyenLieuDAO _nguyenLieuDAO;
+
         public NguyenLieuController(string connectionString)
         {
-            _connectionString = connectionString;
+            _nguyenLieuDAO = new NguyenLieuDAO(connectionString);
         }
+
         public List<NguyenLieuModel> LayDanhSachNguyenLieu()
         {
-            List<NguyenLieuModel> danhSachNguyenLieu = new List<NguyenLieuModel>();
-
-            using (SqlConnection connection = new SqlConnection(_connectionString))
-            {
-                string query = "SELECT * FROM tblNguyenlieu";
-
-                using (SqlCommand command = new SqlCommand(query, connection))
-                {
-                    connection.Open();
-                    using (SqlDataReader reader = command.ExecuteReader())
-                    {
-                        while (reader.Read())
-                        {
-                            danhSachNguyenLieu.Add(new NguyenLieuModel
-                            {
-                                NguyenLieuID = Convert.ToInt32(reader["PK_iNguyenlieuID"]),
-                                TenNguyenLieu = reader["sTenNguyenlieu"].ToString(),
-                                SoLuong = Convert.ToSingle(reader["fSoluong"]),
-                                DonViTinh = reader["sDonvitinh"].ToString(),
-                            });
-                        }
-                    }
-                }
-            }
-            return danhSachNguyenLieu;
+            return _nguyenLieuDAO.LayDanhSachNguyenLieu();
         }
     }
 }
