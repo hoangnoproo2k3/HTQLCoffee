8327d6aca7f0b05fc6796386f61fc1bd9fc82087
phgmai <110882290+pgmai@users.noreply.github.com>
2024-04-11 21:55:41 +0700

    Update TaiKhoanView.cs

--- HTQLCoffee/View/TaiKhoanView.cs
+++ HTQLCoffee/View/TaiKhoanView.cs
@@ -27,40 +27,16 @@ namespace HTQLCoffee.View
         private void frmTaiKhoan_Load(object sender, EventArgs e)
         {
             taikhoan();
+             btn_chinh.Enabled = false;
+            btn_xoa.Enabled = false;
         }
         private void taikhoan()
         {
             this.taiKhoanController.dulieuCBController("Select PK_iPhanquyenID, sTenquyen from tblPhanquyen ", "sTenquyen", "PK_iPhanquyenID", cb_quyen);
-            this.taiKhoanController.dulieuCBController("Select PK_iNhanvienID, sTenNhanvien from tblNhanvien", "PK_iNhanvienID", "PK_iNhanvienID", cb_maNV);
+            this.taiKhoanController.dulieuCBController("Select PK_iNhanvienID, sTenNhanvien from tblNhanvien", "sTenNhanvien", "PK_iNhanvienID", cb_maNV);
             this.taiKhoanController.layDuLieuController(dgv_TaiKhoan);
 
-        }
-        //private void dgv_TaiKhoan_CellClick(object sender, DataGridViewCellEventArgs e)
-        //{
-        //    List<string> Roles = new List<string>() { ROLES.ADMIN };
-        //    if (!Authorize.Instance.isCheckRole(Roles))
-        //    {
-        //        MessageBox.Show("Bạn không có quyền truy cập, vui lòng liên hệ quản trị viên để cấp quyền", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
-        //        return;
-        //    }
-        //    int index = e.RowIndex;
-        //    DataGridViewRow dr = dgv_TaiKhoan.Rows[index];
-        //    tb_TenDangNhap.Text = dr.Cells["dgv_tb_TenDangNhap"].Value.ToString();
-        //    tb_MatKhau.Text = dr.Cells["dgv_tb_MatKhau"].Value.ToString();
-        //    cb_MaNhanVien.SelectedValue = dr.Cells["dgv_tb_MaNhanVien"].Value.ToString();
-        //    cb_LoaiTaiKhoan.SelectedValue = dr.Cells["dgv_tb_LoaiTaiKhoan"].Value.ToString();
-        //    cb_MaNhanVien.Enabled = false;
-        //    btnSuaTaiKhoan.Enabled = true;
-        //    if ((Boolean)dr.Cells["dgv_tb_TrangThai"].Value == true)
-        //    {
-        //        btnKhoaTaiKhoan.Enabled = true;
-        //    }
-        //    else
-        //    {
-        //        btnKhoaTaiKhoan.Enabled = false;
-        //    }
-
-        //}
+        }      
 
         private void btnLammoi_Click(object sender, EventArgs e)
         {
@@ -102,6 +78,7 @@ namespace HTQLCoffee.View
             cb_quyen.SelectedValue = dr.Cells["PK_iPhanquyenID"].Value.ToString();
             cb_maNV.SelectedValue = dr.Cells["PK_iNhanvienID"].Value.ToString();
             cb_maNV.Enabled = false;
+            btn_chinh.Enabled = true;
         }
 
         private void btn_tim_Click(object sender, EventArgs e)
